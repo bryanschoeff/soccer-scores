09ad239fd31d4188c154ca43a57283c36bc00b43
Bryan Schoeff <schoeff@gmail.com>
2013-02-06 21:36:53 -0500

    refactored methds out of discrete tasks in current scores

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -4,41 +4,53 @@ require 'open-uri'
 class Fetcher
 
   def self.get_current_scores
-
-    results = Array.new
-
     page = Nokogiri::HTML(open("http://soccernet.espn.go.com/scores?cc=5901"), nil, 'UTF-8')
-    nbsp = Nokogiri::HTML('&nbsp;')
+    get_leagues(page)
+  end
 
-    # Factor out this block
+  private
+
+  def self.get_leagues(page)
+    results = Array.new
     page.css('div.group-set').each do |league_html|
 
-      league = {}
-
-      league[:name] = league_html.css('h2 a').text
-      league[:url] = league_html.css('h2 a').attr('href').content
-      league[:matches] = []
-
-      # Factor out this block
-      league_html.css('tr').each do |match_html|
-        if match_html.attr('id')
-
-          match = {}
-
-          match[:status] = match_html.css('td.status').text
-          match[:home_team] = match_html.css('td.home_team').text
-          match[:away_team] = match_html.css('td.away_team').text
-          match[:home_score] = match_html.css('td.scores a').text.split('-')[0].to_s.gsub(nbsp, '').to_i
-          match[:away_score] = match_html.css('td.scores a').text.split('-')[1].to_s.gsub(nbsp, '').to_i
-          match[:url] = match_html.css('td.scores a').attr('href').content
-
-          league[:matches] << match
-
-        end
-      end
-      results << league
+    results << get_league(league_html)
     end
-
     results
   end
+
+  def self.get_league(league_html)
+    league = {}
+
+    league[:name] = league_html.css('h2 a').text
+    league[:url] = league_html.css('h2 a').attr('href').content
+    league[:matches] = get_matches(league_html)
+
+    league
+  end
+
+  def self.get_matches(league_html)
+    matches = []
+    league_html.css('tr').each do |match_html|
+      if match_html.attr('id')
+        matches << get_match(match_html)
+      end
+    end
+    matches
+  end
+
+  def self.get_match(match_html)
+    match = {}
+    nbsp = Nokogiri::HTML('&nbsp;')
+
+    match[:status] = match_html.css('td.status').text
+    match[:home_team] = match_html.css('td.home_team').text
+    match[:away_team] = match_html.css('td.away_team').text
+    match[:home_score] = match_html.css('td.scores a').text.split('-')[0].to_s.gsub(nbsp, '').to_i
+    match[:away_score] = match_html.css('td.scores a').text.split('-')[1].to_s.gsub(nbsp, '').to_i
+    match[:url] = match_html.css('td.scores a').attr('href').content
+
+    match
+  end
+
 end
