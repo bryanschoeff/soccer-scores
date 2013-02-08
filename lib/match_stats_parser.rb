bbcae557a1ceb9a9d9bfff7a260002d3f29a83d3
Bryan Schoeff <schoeff@gmail.com>
2013-02-08 08:46:13 -0500

    finished functionality for match statistics

--- lib/match_stats_parser.rb
+++ lib/match_stats_parser.rb
@@ -1,11 +1,16 @@
 require_relative 'fetcher'
 
 class MatchStatsParser
-  attr_accessor :location, :status, :home_team, :away_team, :home_stats, :away_stats
 
   def self.get_match_statistics(match_id)
     results = {home_stats: {}, away_stats: {}}
     match_stats = Fetcher::get_match_statistics(match_id)
+
+    results[:home_team] = match_stats.css('div.team.away p.team-name a').text
+    results[:away_team] = match_stats.css('div.team.home p.team-name a').text
+    results[:home_score] = match_stats.css('div.score-time p.score').text.split('-')[0].strip
+    results[:away_score] = match_stats.css('div.score-time p.score').text.split('-')[1].strip
+    results[:status] = match_stats.css('div.score-time p.time').text
 
     # this could be refactored to be more DRY; it doesn't seem necessary right now
     results[:home_stats][:shots] = match_stats.css('#home-shots').text
@@ -37,7 +42,6 @@ class MatchStatsParser
   private
 
   def self.get_players_stats(players_html)
-
     players_stats = Array.new
     line_up = 'Line-Up'
 
@@ -46,28 +50,32 @@ class MatchStatsParser
       if (table_row.css('th').count == 1)
         line_up = table_row.css('th').text.strip
       elsif (table_row.css('th').count == 0)
-        player_stats = {}
-        table_cells = table_row.css('td')
-
-        player_stats[:line_up] = line_up
-        player_stats[:position] = table_cells[0].css('p').text if table_cells[0]
-        player_stats[:number] = table_cells[1].text if table_cells[1]
-        player_stats[:name] = table_cells[2].css('a').text.strip if table_cells[2]
-        player_stats[:shots] = table_cells[3].text.strip if table_cells[3]
-        player_stats[:shots_on_goal] = table_cells[4].text.strip if table_cells[4]
-        player_stats[:goals] = table_cells[5].text.strip if table_cells[5]
-        player_stats[:assists] = table_cells[6].text.strip if table_cells[6]
-        player_stats[:offsides] = table_cells[7].text.strip if table_cells[7]
-        player_stats[:fouls_drawn] = table_cells[8].text.strip if table_cells[8]
-        player_stats[:fouls_committed] = table_cells[9].text.strip if table_cells[9]
-        player_stats[:saves] = table_cells[10].text.strip if table_cells[10]
-        player_stats[:yellow_cards] = table_cells[11].text.strip if table_cells[11]
-        player_stats[:red_cards] = table_cells[12].text.strip if table_cells[12]
-
-        players_stats << player_stats
+        players_stats << get_player_stats(table_row, line_up)
       end
     end
     players_stats
+  end
+
+  def self.get_player_stats(player_html, line_up)
+    player_stats = {}
+    stat_columns = player_html.css('td')
+
+    player_stats[:line_up] = line_up
+    player_stats[:position] = stat_columns[0].css('p').text if stat_columns[0]
+    player_stats[:number] = stat_columns[1].text if stat_columns[1]
+    player_stats[:name] = stat_columns[2].css('a').text.strip if stat_columns[2]
+    player_stats[:shots] = stat_columns[3].text.strip if stat_columns[3]
+    player_stats[:shots_on_goal] = stat_columns[4].text.strip if stat_columns[4]
+    player_stats[:goals] = stat_columns[5].text.strip if stat_columns[5]
+    player_stats[:assists] = stat_columns[6].text.strip if stat_columns[6]
+    player_stats[:offsides] = stat_columns[7].text.strip if stat_columns[7]
+    player_stats[:fouls_drawn] = stat_columns[8].text.strip if stat_columns[8]
+    player_stats[:fouls_committed] = stat_columns[9].text.strip if stat_columns[9]
+    player_stats[:saves] = stat_columns[10].text.strip if stat_columns[10]
+    player_stats[:yellow_cards] = stat_columns[11].text.strip if stat_columns[11]
+    player_stats[:red_cards] = stat_columns[12].text.strip if stat_columns[12]
+
+    player_stats
   end
 
 end
